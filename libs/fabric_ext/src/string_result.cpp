7c88a7023969189c21ab43933f1d904961df4d2e
Youyuan Wu <youyuanwu@outlook.com>
2023-02-20 18:08:42 -0800

    use windows2019 in ci. SF cannot start in windows2022.

--- libs/fabric_ext/src/string_result.cpp
+++ libs/fabric_ext/src/string_result.cpp
@@ -6,7 +6,9 @@ namespace servicefabric {
 string_result::string_result(std::wstring str) : str_(str) {}
 
 LPCWSTR STDMETHODCALLTYPE string_result::get_String() {
+#ifdef SF_DEBUG
   BOOST_LOG_TRIVIAL(debug) << "string_result::get_String " << str_;
+#endif
   return str_.c_str();
 }
 
