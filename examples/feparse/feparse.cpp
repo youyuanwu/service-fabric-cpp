ce1422bf2f3be830ff485c21198050d530973248
Youyuan Wu <youyuanwu@outlook.com>
2023-08-15 22:09:52 -0700

    support hex hr for feparse

--- examples/feparse/feparse.cpp
+++ examples/feparse/feparse.cpp
@@ -9,12 +9,13 @@ namespace po = boost::program_options;
 
 int main(int argc, char **argv) {
   std::int64_t input = {};
-
+  std::string hex = {};
   try {
 
     po::options_description desc("Allowed options");
     desc.add_options()("help", "produce help message")(
-        "hr", po::value(&input)->default_value(S_OK), "error code value");
+        "hr", po::value(&input)->default_value(S_OK), "error code value")(
+        "hex", po::value(&hex)->default_value("0x0"), "error code hex");
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
@@ -26,9 +27,12 @@ int main(int argc, char **argv) {
       std::cerr << ss.str();
       return EXIT_FAILURE;
     }
-
     // example hr value: -2147017793;
     HRESULT hr = static_cast<HRESULT>(input);
+    if (hr == 0) {
+      // try parse string
+      hr = static_cast<HRESULT>(std::stoll(hex, nullptr, 16)); // hex base
+    }
     std::string str = sf::get_fabric_error_str(hr);
     std::cout << "FabricError: " << str;
   } catch (const std::exception &e) {
